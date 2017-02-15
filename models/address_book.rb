3b9d55942b9dbafb0b4f2699a078d0d5b3fe587a
Chung <jinchung@lm-nyc-00877011.home>
2017-02-15 17:48:33 -0500

    Checkpoint-19-Address-Bloc-Models

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,24 +1,20 @@
 require_relative 'entry'
 
-   class AddressBook
-     attr_reader :entries
+class AddressBook
+    attr_accessor :entries
 
-     def initialize
-       @entries = []
-     end
- -end
+    def initialize
+        @entries = []
+    end
 
-   def add_entry(name, phone_number, email)
-     # #9
-     index = 0
-     entries.each do |entry|
-     # #10
-       if name < entry.name
-         break
-       end
-       index+= 1
-     end
-     # #11
-     entries.insert(index, Entry.new(name, phone_number, email))
-   end
- end
+    def add_entry(name, phone_number, email)
+        index = 0
+        @entries.each do |entry|
+            if name < entry.name
+                break
+            end
+            index += 1
+        end
+        @entries.insert(index, Entry.new(name, phone_number, email))
+    end
+end
